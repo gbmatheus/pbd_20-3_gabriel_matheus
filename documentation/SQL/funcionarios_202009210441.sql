98c57f0bcb3f32cb39051e4643ca462478199679
Gabriel Matheus <48744509+gbmatheus@users.noreply.github.com>
2020-09-21 05:44:52 -0300

    Atualizando documentação e subindo sql's do banco

---
+++ documentation/SQL/funcionarios_202009210441.sql
@@ -0,0 +1,7 @@
+INSERT INTO public.funcionarios (cod_funcionario,quantidade_filhos,tipo,pessoa_id,salario_id) VALUES 
+('12345678900',2,'MES',1,1)
+,('12345678910',2,'MES',2,2)
+,('12345678911',2,'MES',3,3)
+,('12345678921',2,'HORA',4,4)
+,('12312312300',0,'HORA',5,4)
+;
